7cb4ee7a959d27605c06af029a0237c3c8b39f03
Dimitar Sergev <102225734+DimitarSergeev@users.noreply.github.com>
2023-03-18 17:26:34 +0200

    fix bugs

--- client/src/components/AddProduct.js
+++ client/src/components/AddProduct.js
@@ -1,14 +1,14 @@
 import styles from './AddProduct.module.css'
 
 import { useState } from "react"
-import { useNavigate } from "react-router-dom"
+import { useNavigate } from 'react-router-dom'
 
 import Swal from 'sweetalert2'
 import withReactContent from 'sweetalert2-react-content'
 
 import * as productService from '../services/producService'
 
-export const AddProduct = () =>{
+export const AddProduct = () => {
 
     const [colorData, setColorData] = useState([
         {
@@ -31,7 +31,6 @@ export const AddProduct = () =>{
     })
 
     const navigate = useNavigate()
-
     const changeHandler = (e) => {
         setProductData(state => ({
             ...state,
@@ -101,115 +100,132 @@ export const AddProduct = () =>{
 
     const submitHandler = async (e) => {
         e.preventDefault()
-      
-        MySwal.fire({
-          title: 'Създаване на продукт',
-          timerProgressBar: true,
-          didOpen: () => {
-            Swal.showLoading()
-          },
-        })
-      
+
+
         try {
-          await productService.createProduct(productData)
-          MySwal.close()
-          navigate('/admin-qty-page')
+            await productService.createProduct(productData)
+            setColorData(state => {
+                return [{
+                    img: '',
+                    XS: '',
+                    S: '',
+                    M: '',
+                    L: '',
+                    price: '',
+                    code: '',
+                    images: []
+                }]
+            })
+            setProductData(state => {
+                return {
+                    name: '',
+                    type: 'suits',
+                    products: colorData,
+                    descModel: '',
+                    productInfo: ''
+                }
+            })
+            MySwal.fire({
+                title: <strong>Product was created successfuly</strong>,
+                icon: 'success',
+            })
+            navigate('/')
         } catch (error) {
-          MySwal.fire({
-            title: <strong>{error.message}</strong>,
-            icon: 'error',
-          })
+            MySwal.fire({
+                title: <strong>{error.message}</strong>,
+                icon: 'error',
+            })
         }
-      }
+    }
 
-      
-    return(
+
+    return (
         <div className={styles['container']} >
-        <form className={styles['form-cont']} onSubmit={submitHandler}>  
-            <h1 className={styles['title']}>Добавяне на продукт</h1>
-            <div className={styles['base-info']}>
-                <div className={styles['base-item-cont']}>
-                    <label htmlFor="">Име на продукта</label>
-                    <input type="text" name='name' onChange={changeHandler} required />
+            <form className={styles['form-cont']} onSubmit={submitHandler}>
+                <h1 className={styles['title']}>ADD PRODUCT</h1>
+                <div className={styles['base-info']}>
+                    <div className={styles['base-item-cont']}>
+                        <label htmlFor="">Name</label>
+                        <input type="text" name='name' onChange={changeHandler} value={productData.name} required />
+                    </div>
+                    <div className={styles['base-item-cont']}>
+                        <label htmlFor="type">Type</label>
+                        <select name="type" id="type" onChange={changeHandler} required>
+                            <option value="suits">Suits</option>
+                            <option value="dress">Dress</option>
+                            <option value="pants">Pants</option>
+                            <option value="shirts">Shirts</option>
+                            <option value="accessories">Accessories</option>
+                        </select>
+                    </div>
+                    <div className={styles['base-item-cont']}>
+                        <label htmlFor="type">Description</label>
+                        <textarea id="" cols="30" rows="2" name="descModel" onChange={changeHandler} value={productData.descModel} required></textarea>
+                    </div>
+                    <div className={styles['base-item-cont']}>
+                        <label htmlFor="type">Specifications</label>
+                        <textarea name="productInfo" id="" cols="30" rows="2" onChange={changeHandler} value={productData.productInfo} required></textarea>
+                    </div>
                 </div>
-                <div className={styles['base-item-cont']}>
-                    <label htmlFor="type">Тип на продукта</label>
-                    <select name="type" id="type" onChange={changeHandler} required>
-                        <option value="suits">Комплекти</option>
-                        <option value="dress">Рокли</option>
-                        <option value="pants">Панталони</option>
-                        <option value="shirts">Блузи</option>
-                        <option value="accessories">Аксесоари</option>
-                    </select>
-                </div>
-                <div className={styles['base-item-cont']}>
-                    <label htmlFor="type">Описание на продукта</label>
-                    <textarea id="" cols="30" rows="2" name="descModel" onChange={changeHandler} required></textarea>
-                </div>
-                <div className={styles['base-item-cont']}>
-                    <label htmlFor="type">Характеристики на продукта</label>
-                    <textarea name="productInfo" id="" cols="30" rows="2" onChange={changeHandler} required></textarea>
-                </div>
-            </div>
 
-            <button className={styles['add-color']} onClick={addColorForm}>Добавяне на цвят</button>
+                <button className={styles['add-color']} onClick={addColorForm}>ADD DIFFERENT COLOR</button>
 
-            {colorData.map((colorForm, i) => {
-                return (
-                    <div className={styles['color-cont']} key={i}>
-                        <div className={styles['marketing-item']}>
-                            <label htmlFor="code">Главна снимка</label>
-                            <input type="file" name='img' onChange={(e) => colorChangeHandler(e, i)} required />
-                            <div className={styles['picked-img']}>
-                                <img src={colorData[i].img} alt="" />
-                            </div>
-                        </div>
-                        <div className={styles['qty-cont']}>
-                            <div className={styles['qty-item']}>
-                                <label htmlFor="XS">XS</label>
-                                <input type="number" name='XS' value={colorForm.XS} onChange={(e) => colorChangeHandler(e, i)} required />
-                            </div>
-                            <div className={styles['qty-item']}>
-                                <label htmlFor="S">S</label>
-                                <input type="number" name='S' value={colorForm.S} onChange={(e) => colorChangeHandler(e, i)} required />
-                            </div>
-                            <div className={styles['qty-item']}>
-                                <label htmlFor="M">M</label>
-                                <input type="number" name='M' value={colorForm.M} onChange={(e) => colorChangeHandler(e, i)} required />
-                            </div>
-                            <div className={styles['qty-item']}>
-                                <label htmlFor="L">L</label>
-                                <input type="number" name='L' value={colorForm.L} onChange={(e) => colorChangeHandler(e, i)} required />
-                            </div>
-                        </div>
-                        <div className={styles['marketing-cont']}>
+                {colorData.map((colorForm, i) => {
+                    return (
+                        <div className={styles['color-cont']} key={i}>
                             <div className={styles['marketing-item']}>
-                                <label htmlFor="price">Цена</label>
-                                <input type="number" name='price' value={colorForm.price} onChange={(e) => colorChangeHandler(e, i)} required />
-                            </div>
-                            <div className={styles['marketing-item']}>
-                                <label htmlFor="code">Код на продукта</label>
-                                <input type="text" name='code' value={colorForm.code} onChange={(e) => colorChangeHandler(e, i)} required />
-                            </div>
-                            <div className={styles['marketing-item']}>
-                                <label htmlFor="code">Снимки</label>
-                                <input type="file" name='images' multiple="multiple" onChange={(e) => colorChangeHandler(e, i)} required />
+                                <label htmlFor="code">Main photo</label>
+                                <input type="file" name='img' onChange={(e) => colorChangeHandler(e, i)} required />
                                 <div className={styles['picked-img']}>
-                                    <img src={colorData[i].images[0] || ''} alt="" />
-                                    <img src={colorData[i].images[1] || ''} alt="" />
-                                    <img src={colorData[i].images[2] || ''} alt="" />
+                                    <img src={colorData[i].img} alt="" />
                                 </div>
                             </div>
-                            <button className={styles['remove-color']} onClick={(e) => removeColorForm(e, i)}>X</button>
+                            <div className={styles['qty-cont']}>
+                                <div className={styles['qty-item']}>
+                                    <label htmlFor="XS">XS</label>
+                                    <input type="number" name='XS' value={colorForm.XS} onChange={(e) => colorChangeHandler(e, i)} required />
+                                </div>
+                                <div className={styles['qty-item']}>
+                                    <label htmlFor="S">S</label>
+                                    <input type="number" name='S' value={colorForm.S} onChange={(e) => colorChangeHandler(e, i)} required />
+                                </div>
+                                <div className={styles['qty-item']}>
+                                    <label htmlFor="M">M</label>
+                                    <input type="number" name='M' value={colorForm.M} onChange={(e) => colorChangeHandler(e, i)} required />
+                                </div>
+                                <div className={styles['qty-item']}>
+                                    <label htmlFor="L">L</label>
+                                    <input type="number" name='L' value={colorForm.L} onChange={(e) => colorChangeHandler(e, i)} required />
+                                </div>
+                            </div>
+                            <div className={styles['marketing-cont']}>
+                                <div className={styles['marketing-item']}>
+                                    <label htmlFor="price">Price</label>
+                                    <input type="number" name='price' value={colorForm.price} onChange={(e) => colorChangeHandler(e, i)} required />
+                                </div>
+                                <div className={styles['marketing-item']}>
+                                    <label htmlFor="code">Code</label>
+                                    <input type="text" name='code' value={colorForm.code} onChange={(e) => colorChangeHandler(e, i)} required />
+                                </div>
+                                <div className={styles['marketing-item']}>
+                                    <label htmlFor="code">Photos</label>
+                                    <input type="file" name='images' multiple="multiple" onChange={(e) => colorChangeHandler(e, i)} required />
+                                    <div className={styles['picked-img']}>
+                                        <img src={colorData[i].images[0] || ''} alt="" />
+                                        <img src={colorData[i].images[1] || ''} alt="" />
+                                        <img src={colorData[i].images[2] || ''} alt="" />
+                                    </div>
+                                </div>
+                                <button className={styles['remove-color']} onClick={(e) => removeColorForm(e, i)}>X</button>
+                            </div>
                         </div>
-                    </div>
-                )
-            })
-            }
+                    )
+                })
+                }
 
-            <button className={styles['create-btn']} >Създай</button>
-        </form>
+                <button className={styles['create-btn']} >CREATE</button>
+            </form>
 
-    </div>
+        </div>
     )
 }
